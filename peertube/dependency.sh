6f2adeb4e3ffd9f9b533bf4d710865e856367cea
Jim Chen <jimchen4214@gmail.com>
2024-08-04 06:48:33 +0800

    Updates

--- peertube/dependency.sh
+++ peertube/dependency.sh
@@ -1,10 +1,11 @@
 sudo apt update && sudo apt upgrade -y
 apt-get install curl sudo unzip vim
 curl -fsSL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh && sudo -E bash nodesource_setup.sh
+sudo add-apt-repository ppa:redislabs/redis && sudo apt-get update && sudo apt-get install redis
 sudo apt-get install -y nodejs
 sudo npm -g install yarn
 sudo apt install python3-dev python3-pip python-is-python3
-sudo apt install certbot nginx ffmpeg postgresql postgresql-contrib openssl g++ make redis-server git cron wget
+sudo apt install certbot nginx ffmpeg postgresql postgresql-contrib openssl g++ make git cron wget
 sudo systemctl enable --now redis-server postgresql
 
 
