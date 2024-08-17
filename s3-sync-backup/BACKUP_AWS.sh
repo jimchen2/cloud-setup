42190b0fa7947b3ac4ec6cd165063ec2f9755ba1
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2024-08-17 21:43:00 +0800

    Update BACKUP_AWS.sh

--- s3-sync-backup/BACKUP_AWS.sh
+++ s3-sync-backup/BACKUP_AWS.sh
@@ -1,11 +1,11 @@
-rclone copy s3:jimchen4214-archive r2:jimchen4214-archive --progress --transfers 20
-rclone copy s3:jimchen4214-blog r2:jimchen4214-blog --progress --transfers 20
-rclone copy s3:jimchen4214-git r2:jimchen4214-git --progress --transfers 20
-rclone copy s3:jimchen4214-mail r2:jimchen4214-mail --progress --transfers 20
-rclone copy s3:jimchen4214-mongo r2:jimchen4214-mongo --progress --transfers 20
-rclone copy s3:jimchen4214-photo r2:jimchen4214-photo --progress --transfers 20
-rclone copy s3:jimchen4214-status r2:jimchen4214-status --progress --transfers 20
-rclone copy s3:jimchen4214-mastodon r2:jimchen4214-mastodon --progress --transfers 20
+rclone sync s3:jimchen4214-archive r2:jimchen4214-archive --progress --transfers 20
+rclone sync s3:jimchen4214-blog r2:jimchen4214-blog --progress --transfers 20
+rclone sync s3:jimchen4214-git r2:jimchen4214-git --progress --transfers 20
+rclone sync s3:jimchen4214-mail r2:jimchen4214-mail --progress --transfers 20
+rclone sync s3:jimchen4214-mongo r2:jimchen4214-mongo --progress --transfers 20
+rclone sync s3:jimchen4214-photo r2:jimchen4214-photo --progress --transfers 20
+rclone sync s3:jimchen4214-status r2:jimchen4214-status --progress --transfers 20
+rclone sync s3:jimchen4214-mastodon r2:jimchen4214-mastodon --progress --transfers 20
 
 
 #rclone copy s3:jimchen4214-tube r2:jimchen4214-tube --progress --transfers 20
