1b1ed4c425751c3a5acc411435533a067fc30e04
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2024-08-18 03:12:04 +0800

    Update BACKUP_AWS.sh

--- s3-sync-backup/BACKUP_AWS.sh
+++ s3-sync-backup/BACKUP_AWS.sh
@@ -6,9 +6,6 @@ rclone sync s3:jimchen4214-mongo r2:jimchen4214-mongo --progress --transfers 20
 rclone sync s3:jimchen4214-photo r2:jimchen4214-photo --progress --transfers 20
 rclone sync s3:jimchen4214-status r2:jimchen4214-status --progress --transfers 20
 rclone sync s3:jimchen4214-mastodon r2:jimchen4214-mastodon --progress --transfers 20
-
-
-
-rclone sync r2:jimchen4214-tube s3:jimchen4214-tube --progress --transfers 20
+rclone sync s3:jimchen4214-tube r2:jimchen4214-tube --progress --transfers 20
 
 #rclone copy s3:jimchen4214-public r2:jimchen4214-public --progress --transfers 20
